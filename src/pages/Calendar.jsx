98c55e3321bc71dba2e6d53c69e4cd8844266153
Sai Abhiram <132103875+Saiabhiram126@users.noreply.github.com>
2026-01-22 17:58:54 +0530

    Update Calendar.jsx

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,6 +1,6 @@
 import PageHeader from '../components/layout/PageHeader';
 
-const Events = () => (
+const Calender = () => (
     <div className='py-8'>
         <PageHeader title='Academic Calender' subtitle='Upcoming university calender' />
         <div className='flex flex-col items-center justify-center py-24 border-2 border-dashed border-slate-100 rounded-3xl bg-slate-50/50'>
