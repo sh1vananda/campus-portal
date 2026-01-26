80a0710258fa810396218b684bd61f4444551745
Pathan Abdul Mosin Khan <114325519+mosin-23@users.noreply.github.com>
2026-01-26 17:32:12 +0530

    Update CourseAllocation.jsx

--- src/pages/teacher/CourseAllocation.jsx
+++ src/pages/teacher/CourseAllocation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import PageHeader from "../components/layout/PageHeader";
+import PageHeader from '../../components/layout/PageHeader';
 
 const CourseAllocation = () => {
   const [form, setForm] = useState({
